e7f001b236923f9d9f4c7b7a1cafd1435abf8e74
Krzysztof Baranski <krisbaranski@gmail.com>
2021-09-05 22:53:21 +0200

    refactor: Change components name and import

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import About from './components/About';
-import Form from './components/Form';
+import MahakalaForm from './components/MahakalaForm';
 import Statues from './components/Statues';
-import Contact from './components/Contact';
+import ContactForm from './components/ContactForm';
 import Footer from './components/Footer';
 import Impressum from './components/Impressum';
-import PrivPol from './components/PrivPol';
+import PrivatePolicy from './components/PrivatePolicy';
 import DesktopMenu from './components/DesktopMenu';
 
 function App() {
@@ -19,11 +19,11 @@ function App() {
         <Route component={DesktopMenu} />
         <Route path="/mahakala-art" exact component={Home} />
         <Route path="/about" component={About} />
-        <Route path="/form" component={Form} />
+        <Route path="/form" component={MahakalaForm} />
         <Route path="/statues" component={Statues} />
-        <Route path="/contact" component={Contact} />
+        <Route path="/contact" component={ContactForm} />
         <Route path="/impressum" component={Impressum} />
-        <Route path="/privacy-policy" component={PrivPol} />
+        <Route path="/privacy-policy" component={PrivatePolicy} />
         <Footer />
       </div>
     </Router>
