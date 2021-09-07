ea3297e0d5419ec4d1ae8379d91e99744bd35cbc
Krzysztof Baranski <krisbaranski@gmail.com>
2021-09-07 16:36:13 +0200

    docs: Add data files to create props and reusable components

---
+++ src/data/policyData.js
@@ -0,0 +1,40 @@
+const policyData = [
+  {
+    title: '1. Benennung der verantwortlichen Stelle',
+    content:
+      'Die verantwortliche Stelle entscheidet allein oder gemeinsam mit anderen über die Zwecke und Mittel der Verarbeitung von personenbezogenen Daten (z.B. Namen, Kontaktdaten o. Ä.)',
+    address:
+      'Verantwortlich für die Datenverarbeitung auf dieser Website ist: Krzysztof Baranski, Charlottenburger Straße 30, 13086 Berlin.',
+  },
+  {
+    title: '2. Widerruf Ihrer Einwilligung zur Datenverarbeitung',
+    content:
+      'Nur mit Ihrer ausdrücklichen Einwilligung sind einige Vorgänge der Datenverarbeitung möglich. Ein Widerruf Ihrer bereits erteilten Einwilligung ist jederzeit möglich. Für den Widerruf genügt eine formlose Mitteilung per E-Mail. Die Rechtmäßigkeit der bis zum Widerruf erfolgten Datenverarbeitung bleibt vom Widerruf unberührt.',
+  },
+  {
+    title: '3. Recht auf Beschwerde bei der zuständigen Aufsichtsbehörde ',
+    content:
+      'Als Betroffener steht Ihnen im Falle eines datenschutzrechtlichen Verstoßes ein Beschwerderecht bei der zuständigen Aufsichtsbehörde zu. Zuständige Aufsichtsbehörde bezüglich datenschutzrechtlicher Fragen ist der Landesdatenschutzbeauftragte des Bundeslandes, in dem sich der Sitz unseres Unternehmens befindet. Der folgende Link stellt eine Liste der Datenschutzbeauftragten sowie deren Kontaktdaten bereit: ',
+    address:
+      'https://www.bfdi.bund.de/DE/Infothek/Anschriften_Links/anschriften_links-node.html.',
+  },
+  {
+    title: '4. Recht auf Datenübertragbarkeit',
+    content:
+      ' Ihnen steht das Recht zu, Daten, die wir auf Grundlage Ihrer Einwilligung oder in Erfüllung eines Vertrags automatisiert verarbeiten, an sich oder an Dritte aushändigen zu lassen. Die Bereitstellung erfolgt in einem maschinenlesbaren Format. Sofern Sie die direkte Übertragung der Daten an einen anderen Verantwortlichen verlangen, erfolgt dies nur, soweit es technisch machbar ist.',
+  },
+  {
+    title: '5. Recht auf Auskunft, Berichtigung, Sperrung, Löschung',
+    content:
+      'Sie haben jederzeit im Rahmen der geltenden gesetzlichen Bestimmungen  das Recht auf unentgeltliche Auskunft über Ihre gespeicherten personenbezogenen Daten, Herkunft der Daten, deren Empfänger und den Zweck der Datenverarbeitung und ggf. ein Recht auf Berichtigung, Sperrung oder Löschung dieser Daten. Diesbezüglich und auch zu weiteren Fragen zum Thema personenbezogene Daten können Sie sich jederzeit über die im Impressum aufgeführten Kontaktmöglichkeiten an uns wenden.',
+  },
+  {
+    title: '6. Speicherdauer von Beiträgen und Kommentaren',
+    content:
+      'Beiträge und Kommentare sowie damit in Verbindung stehende Daten, wie beispielsweise IP-Adressen, werden gespeichert. Der Inhalt verbleibt auf unserer Website, bis er vollständig gelöscht wurde oder aus rechtlichen Gründen gelöscht werden musste. Die Speicherung der Beiträge und Kommentare erfolgt auf Grundlage Ihrer Einwilligung (Art. 6 Abs. 1 lit. a DSGVO). Ein Widerruf Ihrer bereits erteilten Einwilligung ist jederzeit möglich. Für den Widerruf genügt eine formlose Mitteilung per E-Mail. Die Rechtmäßigkeit bereits erfolgter Datenverarbeitungsvorgänge bleibt vom Widerruf unberührt.',
+    address:
+      'Quelle: https://www.mein-datenschutzbeauftragter.de/ Datenschutz-Konfigurator',
+  },
+];
+
+export default policyData;
