865eb994097bb20bbe7c084e70a28cbe8cfe0114
Krzysztof Baranski <krisbaranski@gmail.com>
2021-09-07 16:38:20 +0200

    fix: Change hardcoded components and add props, reusable components

---
+++ src/components/SingleImpressum.js
@@ -0,0 +1,9 @@
+const SingleImpressum = props => {
+  return (
+    <div>
+      <h5>{props.title}</h5>
+      <p>{props.content}</p>
+    </div>
+  );
+};
+export default SingleImpressum;
---
+++ src/components/SinglePolicy.js
@@ -0,0 +1,10 @@
+const SinglePolicy = props => {
+  return (
+    <div>
+      <h5>{props.title}</h5>
+      <p>{props.content}</p>
+      <p>{props.address}</p>
+    </div>
+  );
+};
+export default SinglePolicy;
