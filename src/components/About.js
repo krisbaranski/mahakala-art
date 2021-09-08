8ae53398267613bfa9675068b69055f5b9328e3d
Krzysztof Baranski <krisbaranski@gmail.com>
2021-09-08 22:34:18 +0200

    style: Adjust text element to right

--- src/components/About.js
+++ src/components/About.js
@@ -2,65 +2,63 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="container about">
-      <div>
-        <h3>
-          Mahakala
-          <p>
-            <em>Black Coat</em> (eng) <em>Ber Nag Chen</em> (tib)
-          </p>
-        </h3>
+    <div className="container">
+      <h3>
+        Mahakala
         <p>
-          “Mahakala is the best known protector in Tibetan Buddhism. Although
-          his appearance is powerfully protective, his essence is compassion. He
-          conquers hindrances that may appear on the path to enlightenment, and
-          appears in various forms, with two, four or six arms. Two-armed forms
-          are emanations of Vajrapani (Diamond in Hand eng, Chan na Dor je tib),
-          four-armed forms are emanations of Manjushri (Wisdom Buddha eng, Jam
-          pal yang tib) and six-armed forms are emanations of Avalokiteshvara
-          (Loving Eyes eng, Chen re sig tib). A special form of the two-armed
-          Mahakala, particularly honoured in Mongolia, is the Panjara-Mahakala
-          (Gor gyi gon po tib), who protects tents and houses.
+          <em>Black Coat</em> (eng) <em>Ber Nag Chen</em> (tib)
         </p>
-        <p>
-          Mahakala’s body colour expresses different emphases of his activity.
-          White forms express pacifying and sometimes increasing activity,
-          whereas golden or yellow forms express only the increasing activity.
-          Red forms express fascinating activity, and the “cutting through”
-          protective activity is symbolised by blue or black.
-        </p>
-        <p>
-          The two-armed Mahakala Bernagchen is the powerfully protective
-          emanation of Diamond in Hand. He is fully in the service of the
-          Karmapa and is the main protector of the Karma Kagyu lineage.
-        </p>
-        <p>
-          The invocation of the protector Black Coat, composed by the 8th
-          Karmapa Mikyo Dorje, is a fundamental meditation of the Karma Kagyu
-          lineage. When practised daily, the invocation protects from obstacles
-          and promotes the development of fearlessness.
-        </p>
-        <p>
-          The two-armed Mahakala Bernagchen has blue-black skin, and is depicted
-          in the Karma Kagyu lineage in a jumping posture on a lotus throne with
-          a sun disk. The human being (sometimes two) he tramples under his feet
-          show that he conquered all disturbances and hindrances. The right hand
-          of this powerful form is raised and holds a chopping knife with the
-          blade turned away from him. In his left hand he holds a skull bowl
-          filled with the blood of the ego in front of his heart. His head is
-          one third of his entire height, and topped by flame like hair standing
-          on end and a crown of five skulls with a dorje at its peak. Above his
-          two natural eyes and bushy eyebrows he bears a vertical wisdom eye on
-          his forehead. His snarling mouth shows four fangs, and is framed by a
-          flaming orange beard. He wears the ornaments of powerful protection,
-          such as jewellery made out of bone and skulls, and is surrounded by
-          wisdom flames.”
-        </p>
-        <span>
-          from
-          <em>Space & Bliss – Buddhist Statues & Ritual Implements</em>
-        </span>
-      </div>
+      </h3>
+      <p>
+        “Mahakala is the best known protector in Tibetan Buddhism. Although his
+        appearance is powerfully protective, his essence is compassion. He
+        conquers hindrances that may appear on the path to enlightenment, and
+        appears in various forms, with two, four or six arms. Two-armed forms
+        are emanations of Vajrapani (Diamond in Hand eng, Chan na Dor je tib),
+        four-armed forms are emanations of Manjushri (Wisdom Buddha eng, Jam pal
+        yang tib) and six-armed forms are emanations of Avalokiteshvara (Loving
+        Eyes eng, Chen re sig tib). A special form of the two-armed Mahakala,
+        particularly honoured in Mongolia, is the Panjara-Mahakala (Gor gyi gon
+        po tib), who protects tents and houses.
+      </p>
+      <p>
+        Mahakala’s body colour expresses different emphases of his activity.
+        White forms express pacifying and sometimes increasing activity, whereas
+        golden or yellow forms express only the increasing activity. Red forms
+        express fascinating activity, and the “cutting through” protective
+        activity is symbolised by blue or black.
+      </p>
+      <p>
+        The two-armed Mahakala Bernagchen is the powerfully protective emanation
+        of Diamond in Hand. He is fully in the service of the Karmapa and is the
+        main protector of the Karma Kagyu lineage.
+      </p>
+      <p>
+        The invocation of the protector Black Coat, composed by the 8th Karmapa
+        Mikyo Dorje, is a fundamental meditation of the Karma Kagyu lineage.
+        When practised daily, the invocation protects from obstacles and
+        promotes the development of fearlessness.
+      </p>
+      <p>
+        The two-armed Mahakala Bernagchen has blue-black skin, and is depicted
+        in the Karma Kagyu lineage in a jumping posture on a lotus throne with a
+        sun disk. The human being (sometimes two) he tramples under his feet
+        show that he conquered all disturbances and hindrances. The right hand
+        of this powerful form is raised and holds a chopping knife with the
+        blade turned away from him. In his left hand he holds a skull bowl
+        filled with the blood of the ego in front of his heart. His head is one
+        third of his entire height, and topped by flame like hair standing on
+        end and a crown of five skulls with a dorje at its peak. Above his two
+        natural eyes and bushy eyebrows he bears a vertical wisdom eye on his
+        forehead. His snarling mouth shows four fangs, and is framed by a
+        flaming orange beard. He wears the ornaments of powerful protection,
+        such as jewellery made out of bone and skulls, and is surrounded by
+        wisdom flames.”
+      </p>
+
+      <span>
+        <em>Space & Bliss – Buddhist Statues & Ritual Implements</em>
+      </span>
     </div>
   );
 };
