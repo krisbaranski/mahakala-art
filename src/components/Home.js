c21f8cd5539d81ccd05f96c2c8b1c0fb4bda3b72
Krzysztof Baranski <krisbaranski@gmail.com>
2022-05-25 22:20:34 +0200

    feat: Update package, add additional button and style css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import mahakala from '../img/mahakala_web.png';
-// import mahakala from 'https://images.ctfassets.net/x9ncw0vro2xo/6nsCXs1EELUs2Wuhvhoh0f/fc2d703c476aedd7c8819213ee906054/Maha_Wroc.jpg';
+import { Link } from 'react-router-dom';
+// import mahakala from 'https://images.ctfassets.net/x9ncw0vro2xo/Z66r1CvcuDADvxcFlcQgf/d0c0615c6653e87175dc7cc22fb8ca9c/mahakala-web.png';
 
 const Home = () => {
   return (
     <section className="container">
       <div className="home">
-        <img src={mahakala} alt="Mahakala Statue" />
+        <img src={mahakala} alt="Mahakala Statue, Black Coat, Ber Nag Chen" />
       </div>
       <div className="home_text">
         <h3>Mahakala</h3>
         <p>Black Coat (eng) Ber Nag Chen (tib)</p>
         <p>44cm height, plaster casting, acrylic</p>
+        <br />
+        <p>
+          available to
+          <Link to="/contact" className="order">
+            order
+          </Link>
+        </p>
       </div>
     </section>
   );
