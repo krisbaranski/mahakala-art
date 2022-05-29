5ce6e1c34122b3a048446d01ae368c70d2b7ada0
Krzysztof Baranski <krisbaranski@gmail.com>
2022-05-29 12:48:00 +0200

    Add button components for mail and download file

---
+++ src/components/MailTo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function MailTo() {
+  const sendMail = () => {
+    window.location.href = 'mailto:info@mahakala-art.com';
+  };
+  return (
+    <div className="home_text">
+      <Link to="#" onClick={sendMail} className="order">
+        contact me
+      </Link>
+    </div>
+  );
+}
