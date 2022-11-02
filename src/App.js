0a53c6ad34386a99de0f5354020e4f29d09b0f43
Krzysztof Baranski <krisbaranski@gmail.com>
2022-11-02 16:14:10 +0100

    refactor: add AnimatedRoutes element, restructure elements of body

--- src/App.js
+++ src/App.js
@@ -1,36 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './components/Home';
-import About from './components/About';
-import MahakalaForm from './components/MahakalaForm';
-import Statues from './components/Statues';
-import ContactForm from './components/ContactForm';
-import Footer from './components/Footer';
-import Impressum from './components/Impressum';
-import PrivacyPolicy from './components/PrivacyPolicy';
-import DesktopMenu from './components/DesktopMenu';
+
 import ScrollToTop from './components/ScrollToTop';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import DesktopMenu from './components/DesktopMenu';
+import AnimatedRoutes from './components/AnimatedRoutes';
 
 function App() {
   return (
     <Router>
       <ScrollToTop>
-        <Switch>
-          <div className="App">
-            <Header />
-            <Route component={DesktopMenu} />
-            <Route path="/" exact component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/form" component={MahakalaForm} />
-            <Route path="/statues" component={Statues} />
-            <Route path="/contact" component={ContactForm} />
-            <Route path="/impressum" component={Impressum} />
-            <Route path="/privacypolicy" component={PrivacyPolicy} />
-            <Footer />
-          </div>
-        </Switch>
+        <Header />
+        <DesktopMenu />
+        <AnimatedRoutes />
+        <Footer />
       </ScrollToTop>
     </Router>
   );
