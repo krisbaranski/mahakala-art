8a63c52c85be09a866b4303c7e113ca821d1427a
Krzysztof Baranski <krisbaranski@gmail.com>
2022-11-02 16:12:49 +0100

    refactor: create new element for framer-motion animation

---
+++ src/components/AnimatedRoutes.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import MahakalaForm from './MahakalaForm';
+import Statues from './Statues';
+import ContactForm from './ContactForm';
+import Impressum from './Impressum';
+import PrivacyPolicy from './PrivacyPolicy';
+
+import { AnimatePresence } from 'framer-motion';
+
+const AnimatedRoutes = () => {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/form" element={<MahakalaForm />} />
+        <Route path="/statues" element={<Statues />} />
+        <Route path="/contact" element={<ContactForm />} />
+        <Route path="/impressum" element={<Impressum />} />
+        <Route path="/privacypolicy" element={<PrivacyPolicy />} />
+      </Routes>
+    </AnimatePresence>
+  );
+};
+
+export default AnimatedRoutes;
