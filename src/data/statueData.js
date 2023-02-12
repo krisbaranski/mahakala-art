48aa7bbdaaedb591b2b26247ed8f8e59742dedc2
Krzysztof Baranski <krisbaranski@gmail.com>
2023-02-12 15:21:54 +0100

    docs: correct text in source data

--- src/data/statueData.js
+++ src/data/statueData.js
@@ -3,8 +3,8 @@ const statueData = [
     id: 0,
     image:
       'https://images.ctfassets.net/x9ncw0vro2xo/PIjMWBbSA1t2xVdO3z5bO/c0e888a12a2cfd957b4bf7ae694509d7/Yum_Chenmo.jpg',
-    title: 'Wisdom of all Buddhas, Yum Chenmo (tib.)',
-    statueDescription: 'The female aspect of Buddhas Wisdom.',
+    title: 'Wisdom of all Buddhas, Yum Chenmo (tib.), Prajnyaparamita',
+    statueDescription: 'The female aspect of Buddhas Wisdom',
     paintedBy: 'Painted by Dawa Lhadipa',
     heightAndYear: '50 cm height, 2011',
     location: 'Diamond Way Retreat Centre Karma Guen, Spain',
@@ -13,8 +13,8 @@ const statueData = [
     id: 1,
     image:
       'https://images.ctfassets.net/x9ncw0vro2xo/6nsCXs1EELUs2Wuhvhoh0f/fc2d703c476aedd7c8819213ee906054/Maha_Wroc.jpg',
-    title: 'Black Coat, Bernagchen (tib.)',
-    statueDescription: 'Active protection aspect of the Lama.',
+    title: 'Black Coat, Bernagchen (tib.), Mahakala (sanskr.)',
+    statueDescription: 'Active protection aspect of the Lama',
     paintedBy: 'Partially painted by Dawa Lhadipa',
     heightAndYear: '96 cm, 2014',
     location: 'Diamond Way Centre Wroclaw, Poland',
@@ -33,7 +33,7 @@ const statueData = [
     id: 3,
     image:
       'https://images.ctfassets.net/x9ncw0vro2xo/5lif2Ta10rs3x6SuHTXsbf/e3a759666f04676a25d90d9265c3c6df/Opame.png',
-    title: 'Buddha of Limitless Light, Opame (tib.)',
+    title: 'Buddha of Limitless Light, Opame (tib.), Amithaba (sanskr.)',
     statueDescription: 'One of five Buddha Families',
     paintedBy: 'Painted by Silke Neukirchen',
     heightAndYear: '108 cm, 2017',
@@ -53,7 +53,7 @@ const statueData = [
     id: 5,
     image:
       'https://images.ctfassets.net/x9ncw0vro2xo/4m2NXWozX7vO9Kz9uValSI/8e1ac697cb965986726a6a6d5a2df0ff/Dorje_Sempa.jpg',
-    title: 'Diamond Mind, Dorje Sempa (tib.)',
+    title: 'Diamond Mind, Dorje Sempa (tib.), Vajrasattva (sanskr.)',
     statueDescription: 'Purification aspect of Buddhas Compassion and Wisdom.',
     paintedBy: 'Painted by Dawa Lhadipa',
     heightAndYear: '150 cm, 2019',
@@ -63,9 +63,10 @@ const statueData = [
     id: 6,
     image:
       'https://images.ctfassets.net/x9ncw0vro2xo/6Wxp0lQLRzmW9IwMQfJkRK/034919258e90ddf4acc8ee92446f27ed/16Karmapa.jpg',
-    title: '16th Karmapa, Rangjung Rigpe Dorje (tib.)',
-    statueDescription: 'Head Teacher of Karma Kamtsang Lineage.',
-    paintedBy: '',
+    title: 'H.H. 16th Karmapa, Rangjung Rigpe Dorje (tib.)',
+    statueDescription:
+      'Head Teacher of Karma Kamtsang Lineage, King of all Yogis',
+    paintedBy: 'Painted by Stanley Krajcy',
     heightAndYear: '120 cm, 2021',
     location: 'Diamond Way Buddhist Centre Prague, Czech Republic',
   },
@@ -74,8 +75,9 @@ const statueData = [
     image:
       'https://images.ctfassets.net/x9ncw0vro2xo/1Nf9rmN75GRcY92hE6aGgX/495171a2759dd594cda52ea880aaeb18/WhiteTara.jpg',
     title: 'White Liberatrice, Sita Tara (tib.)',
-    statueDescription: 'Protecting and long life aspect of the Buddha.',
-    paintedBy: '',
+    statueDescription:
+      'Protecting and long life and compassion aspect of the Buddha',
+    paintedBy: 'Partially painted by Tashi Dorje',
     heightAndYear: '86 cm, 2022',
     location: 'Diamond Way Buddhist Centre Mainz, Germany',
   },
