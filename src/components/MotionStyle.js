f492c2b29d8add46365fdb2345530933892f28d6
Krzysztof Baranski <krisbaranski@gmail.com>
2023-05-03 13:33:45 +0200

    refactor: adjust motion animation, add slidesData

---
+++ src/components/MotionStyle.js
@@ -0,0 +1,7 @@
+const motionStyle = {
+  initial: ' opacity: 0 ',
+  animate: 'opacity: 1 ',
+  exit: 'opacity: 0, transition: { duration: 1 } ',
+};
+
+export default motionStyle;
---
+++ src/data/slidesData.js
@@ -0,0 +1,29 @@
+const slidesData = [
+  {
+    id: 0,
+    url: 'https://images.ctfassets.net/x9ncw0vro2xo/3I2qs29LxYZiYno0sjhNPw/7e2fc1e0f1f374799cd50e687600613b/mahakala_Ole_web.jpg',
+    title: 'Copenhagen',
+  },
+  {
+    id: 1,
+    url: 'https://images.ctfassets.net/x9ncw0vro2xo/4xJOndU4gCWUSS9Ot7HBCJ/76f38e569aa219dab60906bfcf40f96e/mahakala_Danny_web.jpg',
+    title: 'Kossin',
+  },
+  {
+    id: 2,
+    url: 'https://images.ctfassets.net/x9ncw0vro2xo/1CtufCiEwGGhg7ZHAj2KdU/c370e0841853333c88dc417fff87642a/mahakala_Gunnar_web.jpg',
+    title: 'Braunschweig',
+  },
+  {
+    id: 3,
+    url: 'https://images.ctfassets.net/x9ncw0vro2xo/rslpgbIka8h18AB4GiMse/b7146e46f15cb284e79556345dd554b3/mahakala_Moritz_web.jpg',
+    title: 'Munich',
+  },
+  {
+    id: 4,
+    url: 'https://images.ctfassets.net/x9ncw0vro2xo/dBsQ40FFobCCG1LSeHwjx/9a64f1be8b7ef8910b2f3ec55314c101/mahakala_Eta_web.jpg',
+    title: 'Berlin',
+  },
+];
+
+export default slidesData;
